32f1797453db547a5a72e57ad5b9579c39184619
Milo Silva <milo.silva234@gmail.com>
2022-02-14 16:12:49 +1000

    Improve automated remote syncing

--- sync_remote.sh
+++ sync_remote.sh
@@ -1,11 +1,19 @@
-# merge files that exist in dotfiles repo with updated local versions
+# merge files that exist in dotfiles repo dir with latest versions
 rsync \
   -avh \
   --existing \
   --stats \
   --no-implied-dirs \
-  ~/ .
+  $HOME/ .
+
+# update brew file with currently installed software
+brew bundle dump --file $HOME/.dotfiles/Brewfile-local
+
+# Merge locally dumped Brewfile with remote
+sort -m $HOME/.dotfiles/Brewfile $HOME/.dotfiles/Brewfile-local | awk '/^\s*?$/||!seen[$0]++' > Brewfile
+
+# Delete local Brewfile
+rm $HOME/.dotfiles/Brewfile-local
 
 git add .
-
 git commit -m "Sync local configs to remote";
